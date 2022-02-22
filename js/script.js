591e4bcbc9f6a451f349c44020f40a3d9b191572
Anto231996 <tony2396@hotmail.it>
2022-02-22 16:52:19 +0100

    risultato con numeri decimali

--- js/script.js
+++ js/script.js
@@ -6,20 +6,19 @@ let yourAge = parseInt(prompt("Qual'e' la tua eta'"));
 document.getElementById("your-age").innerHTML = yourAge;
 console.log(yourAge);
 
-let ticketPrice = kmToDo * .21;
-document.getElementById("ticket-price").innerHTML = ticketPrice;
-console.log(ticketPrice);
-
+let ticketPrice = ((kmToDo * .21).toFixed(1));
+document.getElementById("ticket-price").innerHTML = ticketPrice + "€";
+console.log(ticketPrice + "€");
 
 let discount20 = (ticketPrice * 20) / 100;
 
 let discount40 = (ticketPrice * 40) / 100;
 
 if (yourAge < 18) {
-    document.getElementById("discount").innerHTML = discount20;
-    console.log(discount20)
+    document.getElementById("discount").innerHTML = discount20 + "€";
+    console.log(discount20 + "€")
   }
   else if (yourAge >= 65) {
-    document.getElementById("discount").innerHTML = discount40;
-    console.log(discount40)
+    document.getElementById("discount").innerHTML = discount40 + "€";
+    console.log(discount40 + "€")
   }
